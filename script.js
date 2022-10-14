5016d646944bb426f3d7f24e2dc52c008925f5da
Roxanne Stahl <109992581+rox-stahl@users.noreply.github.com>
2022-10-14 08:09:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const button = document.querySelector(".button");
+const share = document.querySelector(".share");
+
+button.addEventListener("click", (event) => {
+    
+    share.classList.toggle("hidden");
+});
